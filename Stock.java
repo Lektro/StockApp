75a08b0228d743c5afd3442cf4ad333774cc8046
Elias Khan <38148675+eliashkan@users.noreply.github.com>
2020-06-23 00:58:59 +0200

    Goeie poging. Doet zeker wat het moet doen. Probeer methodes clean te houden.

--- Stock.java
+++ Stock.java
@@ -3,11 +3,24 @@ public class Stock {
 
     static String symbol = "ORCL";
     static String name = "Oracle Corporation";
+    
+    /* 
+        Hardcoded waarden voor je instance variabelen best vermijden.
+        Maak in de app gewoon een Stock obj aan en set deze waarden.
+        Daarvoor zal je setter methodes nodig hebben.
+    */
     static double previousClosingPrice = 34.5;
     static double currentPrice = 34.34;
+    // Je hebt hier een variabele waar je de return value van getPercentageChange zou kunnen opslaan.
+    // Die zou je kunnen afprinten met een toString methode van je object in plaats van sysouts in de methodes te steken.
     static double pricePercentageChange;
 
     // Create new instance of stock, prints out the corresponding class variables.
+    /*
+        Hou je entity klassen clean clean clean!
+        Deze methode heet createNewStock, maar doet ook nog andere dingen.
+        Een nieuwe stock object creëren is de verantwoordelijkheid van een constructor.
+    */
     public static void createNewStock() {
         Stock s = new Stock();
 
@@ -19,6 +32,12 @@ public class Stock {
     }
 
     //  Method to calculate the percentage change between current and previous prices.
+    /*
+        Als je een methode get... noemt dan kan die best het percentage teruggeven als return value.
+        Printen in deze methode maakt dat deze onbruikbaar is voor reuse, volgende keer dat je hem misschien nodig hebt
+        is die sysout irrelevant.
+        Single responsibility!
+    */
     public static void getPercentageChange(){
         pricePercentageChange = 100 * (currentPrice - previousClosingPrice) / previousClosingPrice;
         System.out.println("The percentage change between previous and current prices equals: " + pricePercentageChange + " %");
